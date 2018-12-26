d1d1b0d1dd96ff7b310d8fcc62881717872c4c09
Sujin Park <sujinpark1031@gmail.com>
2018-12-26 11:54:34 -0800

    Complete express routing

--- app.js
+++ app.js
@@ -1,13 +1,18 @@
 //import mongoose
 const mongoose = require('mongoose');
-
 //creates new Express server
 const express = require("express");
-const app = express();
-
 //import my key
 const db = require('./config/keys').mongoURI;
-//connecrt to MongoDB using Mongoose
+//import users and tweets routes
+const users = require("./routes/api/users");
+const tweets = require("./routes/api/tweets");
+//import body parser to parse the JSON we send to our frontend
+const bodyParser = require('body-parser');
+
+
+const app = express();
+//connect to MongoDB using Mongoose
 mongoose
   .connect(db, {
     useNewUrlParser: true
@@ -17,6 +22,12 @@ mongoose
 
 //sets up basic route so we can render info on page
 app.get("/", (req, res) => res.send("G'day Matey!!"));
+app.use("/api/users", users);
+app.use("/api/tweets", tweets);
+app.use(bodyParser.urlencoded({
+  extended: false
+}));
+app.use(bodyParser.json());
 
 //determine which port to run server on (process.env.PORT is what we need our app to run on for deploying to Heroku)
 const port = process.env.PORT || 5000;
