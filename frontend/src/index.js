2bfb322a42ac989305fad067f043fce112cd097a
Sujin Park <sujinpark1031@gmail.com>
2018-12-27 15:18:56 -0800

    Add User Logout

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,9 +1,63 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import axios from 'axios';
 
-ReactDOM.render( < App / > , document.getElementById('root'));
+// We will create this component shortly
+import Root from './components/root';
 
-window.axios = axios;
+// We set this up in the last section
+import configureStore from './store/store';
+
+// We will use this to parse the user's session token
+import jwt_decode from 'jwt-decode';
+
+// The session utility we just created
+import {
+	setAuthToken
+} from './util/session_api_util';
+
+// We have not created this action yet, but will do so in the next step
+import {
+	logout
+} from './actions/session_actions';
+
+document.addEventListener('DOMContentLoaded', () => {
+			let store;
+			// If a returning user has a session token stored in localStorage
+			if (localStorage.jwtToken) {
+
+				//Set token as common header for all axios requests
+				setAuthToken(localStorage.jwtToken);
+
+				//Decode token to obtain user's info
+				const decodedUser = jwt_decode(localStorage.jwtToken);
+
+				//Create preconfigured state that we can immediately add to our store
+				const preloadedState = {
+					session: {
+						isAuthenticated: true,
+						user: decodedUser
+					}
+				};
+
+				store = configureStore(preloadedState);
+
+				const currentTime = Date.now() / 1000;
+
+				//If user's token has expired
+				if (decodedUser.exp < currentTime) {
+					//Logout user and redirect user to login page
+					store.dispatch(logout());
+					window.location.href = '/login';
+				}
+			} else {
+				//If this is first time user, start w empty store
+				store = configureStore({});
+			}
+
+			//Render root component and pass in store as prop
+			const root = document.getElementById('root');
+			ReactDOM.render( < Root store = {
+					store
+				}
+				/>, root);
+			});
--- frontend/src/reducers/root_reducer.js
+++ frontend/src/reducers/root_reducer.js
@@ -2,9 +2,11 @@ import {
 	combineReducers
 } from 'redux';
 
+import sessionReducer from './session_reducer';
+
 
 const RootReducer = combineReducers({
-
+	session: sessionReducer
 });
 
 export default RootReducer;
