e75d655f76537f29e5b8fbf5767dd60b2ac995eb
Sujin Park <sujinpark1031@gmail.com>
2018-12-27 16:08:20 -0800

    Add session form components

--- frontend/src/actions/session_actions.js
+++ frontend/src/actions/session_actions.js
@@ -36,6 +36,9 @@ export const signup = user => dispatch => (
 export const login = user => dispatch => (
 	APIUtil.login(user)
 	.then((res) => {
+		const {
+			token
+		} = res.data;
 		localStorage.setItem('jwtToken', token);
 		APIUtil.setAuthToken(token);
 		const decoded = jwt_decode(token);
